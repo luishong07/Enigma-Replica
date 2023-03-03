0a1b2951c4d633d0946752e043dbab30aabc4fa4
Luis Hong <luishong50@gmail.com>
2023-03-03 15:38:18 -0600

    started the window listener and used useEffect to prevente repeat execution of callback and remove listener

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello there
-        </p>
-      </header>
-    </div>
-  );
+
+	const handler = (e:any) => {
+		// e.preventDefault()
+		console.log(e)
+	}
+	// window.addEventListener("click", handler)
+	useEffect(()=>{
+		window.addEventListener("keydown",handler)
+		return ()=>window.removeEventListener("keydown",handler)
+	},[handler])
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Hello there</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
+
