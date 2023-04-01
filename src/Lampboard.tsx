7a040b013ff6dafdd5ff77023e353eb0ec885ddb
Luis Hong <luishong50@gmail.com>
2023-03-31 20:10:52 -0500

    created lampboard elements for letters with regular html css version

--- src/Lampboard.tsx
+++ src/Lampboard.tsx
@@ -10,29 +10,66 @@ const Lampboard = () => {
 
     return (
         // <div className='container'>
-        <Container fluid className="lamp-container ">
-            <Row className='row-item ' >
-                {
-                    firstRow.map(e => {
-                        return <Col key={e}>{e}</Col>
-                    })
-                }
-            </Row>
-            <Row className='row-item '>
-                {
-                    secondRow.map(e => {
-                        return <Col key={e}>{e}</Col>
-                    })
-                }
-            </Row>
-            <Row className='row-item '>
-                {
-                    thirdRow.map(e => {
-                        return <Col key={e}>{e}</Col>
-                    })
-                }
-            </Row>
-        </Container>
+        // <Container fluid className="lamp-container ">
+        //     <Row className='row-item' >
+        //         {
+        //             firstRow.map(e => {
+        //                 return <Col key={e}>{e}</Col>
+        //             })
+        //         }
+        //     </Row>
+        //     <Row className='row-item'>
+        //         {
+        //             secondRow.map(e => {
+        //                 return <Col key={e}>{e}</Col>
+        //             })
+        //         }
+        //     </Row>
+        //     <Row className='row-item'>
+        //         {
+        //             thirdRow.map(e => {
+        //                 return <Col key={e}>{e}</Col>
+        //             })
+        //         }
+        //     </Row>
+        // </Container>
+
+        <div className="base">
+            <div className="line1">
+                <span>Q</span>
+                <span>W</span>
+                <span>E</span>
+                <span>R</span>
+                <span>T</span>
+                <span>Y</span>
+                <span>U</span>
+                <span>I</span>
+                <span>O</span>
+                <span>P</span>
+            </div>
+            <div className="line2">
+                <span>A</span>
+                <span>S</span>
+                <span>D</span>
+                <span>F</span>
+                <span>G</span>
+                <span>H</span>
+                <span>J</span>
+                <span>K</span>
+                <span>L</span>
+            </div>
+            <div className="line3">
+                <span>Z</span>
+                <span>X</span>
+                <span>C</span>
+                <span>V</span>
+                <span>B</span>
+                <span>N</span>
+                <span>M</span>
+
+            </div>
+
+        </div>
     );
 }
 
