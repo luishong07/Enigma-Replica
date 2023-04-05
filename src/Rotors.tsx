d960cd0f00b760f558cd4aac02154f65cfea3aca
Luis Hong <luishong50@gmail.com>
2023-04-04 22:14:17 -0500

    started structure for showing rotor information

--- src/Rotors.tsx
+++ src/Rotors.tsx
@@ -1,12 +1,25 @@
 import "./Rotors.css"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
+
 
 const Rotors = () => {
 
 
     return (
-        <div className="rotor-container">
-            Rotors
-        </div>
+        <Container fluid className="rotor-container">
+            <Modal.Dialog className="positions">
+
+
+                <Modal.Body>
+                    <p>Modal body text goes here.</p>
+                </Modal.Body>
+
+
+            </Modal.Dialog>
+        </Container>
     )
 }
 
