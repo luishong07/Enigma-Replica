9065b63f329f62f6a58ccc56c3063218513217d8
Luis Hong <luishong50@gmail.com>
2023-08-10 22:42:11 -0500

    changed some styling on textareas

--- src/Rotorboard.tsx
+++ src/Rotorboard.tsx
@@ -24,19 +24,16 @@ const Rotors: React.FC<Props> = ({ leftPosition, middlePosition, rightPosition }
     return (
         <Container fluid className="rotor-container ">
 
-            <Card className="  my-2">
+            <Card className="my-2">
                 <Card.Body>
                     <div>Your Message</div>
                     <div>
-                        <form>
-                            <input
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                type="text"
-                                placeholder="Enter your message here"
-                            />
-
-                        </form>
+                        <textarea
+                            className="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            placeholder="Enter your message here"
+                        />
                     </div>
                 </Card.Body>
             </Card>
@@ -47,6 +44,8 @@ const Rotors: React.FC<Props> = ({ leftPosition, middlePosition, rightPosition }
                             <option>I</option>
                             <option>II</option>
                             <option>III</option>
+                            <option>IV</option>
+                            <option>V</option>
                         </Form.Select>
                         <Button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
                             <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
@@ -63,6 +62,8 @@ const Rotors: React.FC<Props> = ({ leftPosition, middlePosition, rightPosition }
                             <option>I</option>
                             <option>II</option>
                             <option>III</option>
+                            <option>IV</option>
+                            <option>V</option>
                         </Form.Select>
                         <Button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
                             <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
@@ -79,6 +80,8 @@ const Rotors: React.FC<Props> = ({ leftPosition, middlePosition, rightPosition }
                             <option>I</option>
                             <option>II</option>
                             <option>III</option>
+                            <option>IV</option>
+                            <option>V</option>
                         </Form.Select>
                         <Button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
                             <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
@@ -94,7 +97,11 @@ const Rotors: React.FC<Props> = ({ leftPosition, middlePosition, rightPosition }
             <Card className="  my-2">
                 <Card.Body>
                     <div>Your Encrypted Message</div>
-                    <p className="encrypted-message">{message}</p>
+                    <textarea 
+                        readOnly 
+                        className="encrypted-message" 
+                        value={message} placeholder="Your encrypted message"
+                    />
                 </Card.Body>
             </Card>
         </Container>
